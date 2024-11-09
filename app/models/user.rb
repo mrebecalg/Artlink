9fa4d02e47da6470ddcea4d1350894e95ec10ba4
mrebecalg <mrebecalg@gmail.com>
2024-11-08 22:55:40 -0400

    Cambios en la contraseña, complejidad y tamaño minimo

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,17 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  # Custom password complexity validation
+  validate :password_complexity
+
+  private
+
+  def password_complexity
+    if password.present?
+      unless password =~ /\d/ && password =~ /[^A-Za-z0-9]/
+        errors.add :password, "must include at least one number and one special character"
+      end
+    end
+  end
 end
